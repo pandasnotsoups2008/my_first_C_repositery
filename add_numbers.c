f772b21cb722109dda0bd3b527a7a9e6f8428dee
Mayur Navaratna <mayurnavaratna2008@gmail.com>
2026-05-27 10:48:47 +0530

    Add print_integers code and executable dump

--- add_numbers.c
+++ add_numbers.c
@@ -6,6 +6,6 @@ int main()
     int num1, num2;
     scanf("%d %d", &num1, &num2);
     int sum = num1 + num2;
-    printf("Sum = %d\n", sum);    // ← This line is missing!
+    printf("Sum = %d\n", sum);   
     return 0;
 }
---
+++ print_integers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    printf("Enter the two numbers you want to print: ");
+    scanf("%d %d", &num1, &num2);   
+    printf("The numbers you entered are: %d and %d\n", num1, num2);
+    return 0;
+}
